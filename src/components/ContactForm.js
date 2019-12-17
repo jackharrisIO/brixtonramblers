8c74fe0dd492c9087036b0a7e8f7317fcc8c7714
Jack Harris <jack@tallyho.cc>
2019-12-17 22:58:13 +1300

    form fixes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,7 +9,8 @@ export default function ContactForm() {
         <p>
           <strong>If you would</strong> like to chat with us with a view to joining Freemasonry, or the Brixton Ramblers Lodge in particular, drop us a message. We're a friendly bunch and always happy to help.
         </p>
-        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
+        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true" action="/success" data-netlify-honeypot="bot-field" hidden>
+          <input type="hidden" name="bot-field" />
           <div className="fields">
             <div className="field half">
               <input type="text" name="name" id="name" placeholder="Name" />
@@ -23,6 +24,7 @@ export default function ContactForm() {
                 id="message"
                 placeholder="Message"
                 rows="7"
+                required
               />
             </div>
           </div>
@@ -37,26 +39,6 @@ export default function ContactForm() {
             </li>
           </ul>
         </form>
-        <form name="contact" method="POST" data-netlify-recaptcha="true" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
       </div>
     </>
   );
---
+++ src/pages/success.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h2>Thanks for your message!</h2>
+    <h4>The secretary will be in touch shortly.</h4>
+  </div>
+)
