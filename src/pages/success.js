220c9f24fa505283764707f5f21f8860a9c49ea2
Jack Harris <jack@tallyho.cc>
2019-12-21 12:15:19 +1300

    build

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,11 +1,20 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from '../components/Layout';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import SideFooter from '../components/SideFooter';
 
-const ThankYouPage = () => (
+const Success = () => (
   <Layout>
-    <h1>Thanks for your message!</h1>
+  <div className="content">
+    <h2>Thanks for your message!</h2>
     <p>The secretary will be in touch shortly.</p>
+    </div>
+  <section>
+        <SideFooter />
+      </section>
+      <Footer />
   </Layout>
 )
 
-export default ThankYouPage
+export default Success
